96a228ae173f3ee86d8c96a2823e93f2f9bfa4f5
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-11 15:34:32 +0200

    navbar usernav image fix and login logout register button

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -1,32 +1,60 @@
-import React from 'react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
-import { MenuIcon } from "lucide-react"
-import Image from "next/image"
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
+const UserNav = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const userImage = user?.picture || "/user.png";
 
-const UserNav = () => {
   return (
     <DropdownMenu>
-        <DropdownMenuTrigger>
-            <div className="rounded-full border p-2 lg:py-2 lg:px-4 flex items-center gap-x-3">
-                <MenuIcon className="size-6 lg:size-5" />
+      <DropdownMenuTrigger>
+        <div className="rounded-full border p-2 lg:py-2 lg:px-4 flex items-center gap-x-3">
+          <MenuIcon className="size-6 lg:size-5" />
 
-                <Image
-                    alt="User profile"
-                    src="/profileImage.jpg"
-                    width={30}
-                    height={30}
-                    className="rounded-full hidden lg:block"
-                />
-            </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="w-[200px]">
-            <DropdownMenuItem><RegisterLink className="w-full">Register</RegisterLink></DropdownMenuItem>
-            <DropdownMenuItem><LoginLink className="w-full">Sign in</LoginLink></DropdownMenuItem>
-        </DropdownMenuContent>
+          <Image
+            alt="User profile"
+            src={userImage}
+            width={30}
+            height={30}
+            className="rounded-full hidden lg:block"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[200px]">
+        {user ? (
+          <>
+            <DropdownMenuItem>
+              <LogoutLink className="w-full">Logout</LogoutLink>
+            </DropdownMenuItem>
+            
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>
+              <RegisterLink className="w-full">Register</RegisterLink>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <LoginLink className="w-full">Sign in</LoginLink>
+            </DropdownMenuItem>
+          </>
+        )}
+      </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
-export default UserNav
+export default UserNav;
