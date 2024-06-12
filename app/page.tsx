d3b9b77c344d6eb2a1568d694b9afa64244d7a78
Luka Stefanovic <lukastefanoviccurioso@gmail.com>
2024-06-12 14:26:11 +0200

    mapfilteritems done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+import MapFilterItems from "@/components/MapFilterItems";
+
 
 
 export default function Home() {
   return (
-    
-    <Button>Munem</Button>
+    <div className="container mx-auto px-5 lg:px-10">
+      <MapFilterItems />
+    </div>
   );
 }
+
+//44.37
